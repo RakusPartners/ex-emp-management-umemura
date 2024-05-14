7b7026ff755bda1c8640ab4b3f9a86dd3531548b
Reika <reika.umemura@rakus-partners.co.jp>
2024-05-14 14:06:10 +0900

    login.htmlの修正

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import com.example.form.InsertAdministratorForm;
 import com.example.form.LoginForm;
 import com.example.service.AdministratorService;
 
+import jakarta.servlet.http.HttpSession;
+
 /**
  * 管理者登録画⾯を表⽰する処理を記述するcontrollerクラス
  * @author umemura
@@ -18,6 +21,9 @@ import com.example.service.AdministratorService;
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
+
+    @Autowired
+    private HttpSession session;
 
     @Autowired
     private AdministratorService administratorService;
@@ -53,5 +59,17 @@ public class AdministratorController {
     @GetMapping("/")
     public String toLogin(LoginForm form) {
         return "administrator/login";
+    }
+    @PostMapping("/login")
+    public String login(LoginForm form, Model model) {
+        Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
+
+        if(administrator == null) {
+            model.addAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
+            return "administrator/login";
+        } else {
+            session.setAttribute("administratorName", administrator.getName());
+            return "redirect:/employee/showList"; 
+        }
     }
 }
--- src/main/java/com/example/repository/AdministratorRepository.java
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -46,7 +46,7 @@ public class AdministratorRepository {
      * @return 管理者情報
      */
     public Administrator findByMailAddressAndPassword(String mailAddress, String password) {
-        String sql = "SELECT id, name, mail_address, password FROM administrators WHERE mailAddress=:mailAddress AND password=:password";
+        String sql = "SELECT id, name, mail_address, password FROM administrators WHERE mail_address=:mailAddress AND password=:password";
         SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress).addValue("password", password);
         List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER); 
 
